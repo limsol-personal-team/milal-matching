209683d4659aabdbd1388fd1bae4552436d90226
Junho Song <125498177+JunhoSong03@users.noreply.github.com>
2023-02-26 23:29:53 -0500

    Development be junho (#2)
    * added space for testing
    
    * updates to volunteer model
    
    * update readme
    
    * update readme
    
    * UUID
    
    * added space for testing
    
    * UUID
    
    ---------
    
    Co-authored-by: User <user@rushs-mbp.lan>
    Co-authored-by: Sol Lim <lims4010@gmail.com>

--- server/common/models.py
+++ server/common/models.py
@@ -1,6 +1,8 @@
+import uuid
 from django.db import models
 
 class BaseModel(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     class Meta:
-        abstract = True
+        abstract = True    
