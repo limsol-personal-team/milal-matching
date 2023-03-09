a788db0b3e46ca91692d7a8d6dadaaa75b416fcb
JaviThomas <dog2785@gmail.com>
2023-03-08 22:49:26 -0500

    foundation of homepage

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './App.tsx';
 import reportWebVitals from './reportWebVitals';
+import { StyledEngineProvider } from '@mui/material/styles';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <StyledEngineProvider injectFirst>
+    <App/>
+  </StyledEngineProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
